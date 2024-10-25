50d4f2b3302426edcb4a2b3afe81fa66d1314e15
Damien Westerman <damien.westerman@snhu.edu>
2024-10-24 19:37:27 -0600

    Initial repo setup

---
+++ src/main/java/com/damienwesterman/defensedrill/mvc/DefenseDrillMvcApplication.java
@@ -0,0 +1,13 @@
+package com.damienwesterman.defensedrill.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefenseDrillMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefenseDrillMvcApplication.class, args);
+	}
+
+}
