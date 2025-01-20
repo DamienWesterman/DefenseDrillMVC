c403e00dc27dfc278cff4fe038ca9c4752a27eff
Damien Westerman <damien.westerman@snhu.edu>
2025-01-20 15:03:00 -0700

    Finished modify users

--- src/main/java/com/damienwesterman/defensedrill/mvc/service/UserApiService.java
+++ src/main/java/com/damienwesterman/defensedrill/mvc/service/UserApiService.java
@@ -26,6 +26,8 @@
 
 package com.damienwesterman.defensedrill.mvc.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -35,9 +37,10 @@ import org.springframework.web.client.RestTemplate;
 
 import com.damienwesterman.defensedrill.mvc.util.Constants;
 import com.damienwesterman.defensedrill.mvc.web.BackendResponse;
-import com.damienwesterman.defensedrill.mvc.web.dto.DrillResponseDTO;
 import com.damienwesterman.defensedrill.mvc.web.dto.ErrorMessageDTO;
+import com.damienwesterman.defensedrill.mvc.web.dto.UserFormDTO;
 import com.damienwesterman.defensedrill.mvc.web.dto.UserInfoDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -54,6 +57,7 @@ public class UserApiService {
     private final ObjectMapper objectMapper;
 
     private final static String API_ENDPOINT = Constants.SECURITY_API_BASE_ADDRESS;
+    private final static String ID_ENDPOINT = "/id/{id}";
 
     /**
      * TODO: All doc comments
@@ -90,5 +94,162 @@ public class UserApiService {
         }
 
         return new BackendResponse<UserInfoDTO[]>(retStatus, retDto, retError);
+    }
+
+    @NonNull
+    public BackendResponse<UserInfoDTO> create(@NonNull UserFormDTO user) {
+        HttpStatusCode retStatus = null;
+        UserInfoDTO retDto = null;
+        ErrorMessageDTO retError = null;
+        ResponseEntity<String> response =
+            restTemplate.postForEntity(
+                API_ENDPOINT,
+                user,
+                String.class
+            );
+
+        switch((HttpStatus) response.getStatusCode()) {
+            case CREATED:
+                retStatus = HttpStatus.CREATED;
+                // Extract returned object
+                try {
+                    retDto = objectMapper.readValue(response.getBody(), UserInfoDTO.class);
+                } catch (JsonProcessingException e) {
+                    log.error(e.toString());
+                    retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+                    retDto = null;
+                    retError = Constants.GENERIC_INTERNAL_ERROR_DTO;
+                }
+                retError = null;
+                break;
+
+            case BAD_REQUEST:
+                retStatus = HttpStatus.BAD_REQUEST;
+                retDto = null;
+                try {
+                    retError = objectMapper.readValue(response.getBody(), ErrorMessageDTO.class);
+                } catch (JsonProcessingException e) {
+                    log.error(e.toString());
+                    retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+                    retDto = null;
+                    retError = Constants.GENERIC_INTERNAL_ERROR_DTO;
+                }
+                break;
+
+            case INTERNAL_SERVER_ERROR:
+                // Fallthrough intentional
+            default:
+                retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+                retDto = null;
+                retError = Constants.GENERIC_INTERNAL_ERROR_DTO;
+                break;
+        }
+
+        return new BackendResponse<UserInfoDTO>(retStatus, retDto, retError);
+    }
+
+    @NonNull
+    public BackendResponse<UserInfoDTO> get(@NonNull Long id) {
+        HttpStatusCode retStatus = null;
+        UserInfoDTO retDto = null;
+        ErrorMessageDTO retError = null;
+        ResponseEntity<UserInfoDTO> response =
+            restTemplate.getForEntity(
+                API_ENDPOINT + ID_ENDPOINT,
+                UserInfoDTO.class,
+                id
+            );
+
+        switch((HttpStatus) response.getStatusCode()) {
+            case OK:
+                retStatus = HttpStatus.OK;
+                retDto = response.getBody();
+                retError = null;
+                break;
+
+            case NOT_FOUND:
+                retStatus = HttpStatus.NOT_FOUND;
+                retDto = null;
+                retError = new ErrorMessageDTO(
+                    Constants.NOT_FOUND_ERROR,
+                    "User " + id + " does not exist."
+                );
+                break;
+
+            case INTERNAL_SERVER_ERROR:
+                // Fallthrough intentional
+            default:
+                retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+                retDto = null;
+                retError = Constants.GENERIC_INTERNAL_ERROR_DTO;
+                break;
+        }
+
+        return new BackendResponse<UserInfoDTO>(retStatus, retDto, retError);
+    }
+    @NonNull
+    public BackendResponse<UserInfoDTO> update(@NonNull Long id, @NonNull UserFormDTO user) {
+        HttpStatusCode retStatus = null;
+        UserInfoDTO retDto = null;
+        ErrorMessageDTO retError = null;
+        ResponseEntity<String> response =
+            restTemplate.postForEntity(
+                API_ENDPOINT + ID_ENDPOINT,
+                user,
+                String.class,
+                id
+            );
+
+        switch((HttpStatus) response.getStatusCode()) {
+            case OK:
+                retStatus = HttpStatus.OK;
+                // Extract returned object
+                try {
+                    retDto = objectMapper.readValue(response.getBody(), UserInfoDTO.class);
+                } catch (JsonProcessingException e) {
+                    log.error(e.toString());
+                    retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+                    retDto = null;
+                    retError = Constants.GENERIC_INTERNAL_ERROR_DTO;
+                }
+                retError = null;
+                break;
+
+            case BAD_REQUEST:
+                retStatus = HttpStatus.BAD_REQUEST;
+                retDto = null;
+                try {
+                    retError = objectMapper.readValue(response.getBody(), ErrorMessageDTO.class);
+                } catch (JsonProcessingException e) {
+                    log.error(e.toString());
+                    retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+                    retDto = null;
+                    retError = Constants.GENERIC_INTERNAL_ERROR_DTO;
+                }
+                break;
+
+            case NOT_FOUND:
+                retStatus = HttpStatus.NOT_FOUND;
+                retDto = null;
+                retError = new ErrorMessageDTO(
+                    Constants.NOT_FOUND_ERROR,
+                    "User " + id + " does not exist."
+                );
+                break;
+
+            case INTERNAL_SERVER_ERROR:
+                // Fallthrough intentional
+            default:
+                retStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+                retDto = null;
+                retError = Constants.GENERIC_INTERNAL_ERROR_DTO;
+                break;
+        }
+
+        return new BackendResponse<UserInfoDTO>(retStatus, retDto, retError);
+    }
+
+    public void delete(@NonNull Long id) {
+        restTemplate.delete(API_ENDPOINT + ID_ENDPOINT, id);
     }
 }
--- src/main/java/com/damienwesterman/defensedrill/mvc/web/controller/HtmxUserController.java
+++ src/main/java/com/damienwesterman/defensedrill/mvc/web/controller/HtmxUserController.java
@@ -37,6 +37,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -99,6 +100,23 @@ public class HtmxUserController {
         return "layouts/htmx/view_window_list :: viewWindowList";
     }
 
+    @GetMapping("/view/{id}")
+    public String viewOneUser(Model model, @PathVariable Long id) {
+        var response = apiService.get(id);
+        if (response.hasError()) {
+            model.addAttribute("errorMessage", response.getError().toString());
+            return viewAllUsers(model);
+        }
+
+        var user = response.getResponse();
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("id", id);
+        model.addAttribute("roles", user.getRoles());
+        model.addAttribute("backEndpoint", "/htmx/user/view");
+
+        return "layouts/htmx/user_view_one :: userDetails";
+    }
+
     @GetMapping("/create")
     public String createUserForm(Model model) {
         model.addAttribute("windowTitle", "Create New User");
@@ -114,8 +132,161 @@ public class HtmxUserController {
     }
 
     @PostMapping("/create")
-    @ResponseBody
     public String createUser(Model model, @ModelAttribute UserFormDTO user) {
-        return user.toString();
+        var response = apiService.create(user);
+        if (response.hasError()) {
+            model.addAttribute("errorMessage", response.getError().toString());
+            return createUserForm(model);
+        }
+
+        model.addAttribute("successMessage", "User Created Successfully!");
+
+        var newUser = response.getResponse();
+        model.addAttribute("username", newUser.getUsername());
+        model.addAttribute("id", newUser.getId());
+        model.addAttribute("roles", newUser.getRoles());
+        model.addAttribute("backEndpoint", "/htmx/user/create");
+
+        return "layouts/htmx/user_view_one :: userDetails";
+    }
+
+    @GetMapping("/modify")
+    public String modifyUsersList(Model model) {
+        var response = apiService.getAll();
+        if (response.hasError()) {
+            model.addAttribute("errorMessage", response.getError().toString());
+            model.addAttribute("windowTitle", "Error");
+            model.addAttribute("buttonText", "");
+            model.addAttribute("listItems", Map.of());
+        } else {
+            List<UserInfoDTO> users = List.of(response.getResponse());
+
+            model.addAttribute("windowTitle",
+                "Choose User to Modify");
+            model.addAttribute("buttonText", "Modify");
+
+            // Add users to list
+            BiFunction<String, Long, Map<String, String>> createListItem =
+                (description, userId) -> Map.of(
+                    "itemDescription", description,
+                    "htmxEndpoint", "/htmx/user/modify/" + userId
+                );
+            List<Map<String, String>> listItems = new ArrayList<>(users.size());
+            for (var user : users) {
+                listItems.add(
+                    createListItem.apply(user.getUsername(), user.getId())
+                );
+            }
+            model.addAttribute("listItems", listItems);
+        }
+
+        return "layouts/htmx/view_window_list :: viewWindowList";
+    }
+    @GetMapping("/modify/{id}")
+    public String modifyOneUserForm(Model model, @PathVariable Long id) {
+        var response = apiService.get(id);
+        if (response.hasError()) {
+            model.addAttribute("errorMessage", response.getError().toString());
+            return modifyUsersList(model);
+        }
+
+        UserInfoDTO user = response.getResponse();
+
+        model.addAttribute("windowTitle", "Modify User: " + id);
+        model.addAttribute("postEndpoint", "/htmx/user/modify/" + id);
+        model.addAttribute("buttonText", "Update");
+        model.addAttribute("usernameText", user.getUsername());
+
+        List<Pair<String, Boolean>> rolesList = Constants.ALL_ROLES_LIST.stream()
+            .map(role -> {
+                Boolean checked = Boolean.FALSE;
+                if (user.getRoles().contains(role)) {
+                    checked = Boolean.TRUE;
+                }
+                return new Pair<String, Boolean>(role, checked);
+            })
+            .collect(Collectors.toList());
+        model.addAttribute("rolesList", rolesList);
+
+        return "layouts/htmx/user_form :: userForm";
+    }
+
+    @PostMapping("/modify/{id}")
+    public String modifyOneUser(Model model,
+            @PathVariable Long id, @ModelAttribute UserFormDTO user) {
+        var response = apiService.update(id, user);
+        if (response.hasError()) {
+            model.addAttribute("errorMessage", response.getError().toString());
+            return modifyUsersList(model);
+        }
+
+        model.addAttribute("successMessage", "User Modified Successfully!");
+
+        var updatedUser = response.getResponse();
+        model.addAttribute("username", updatedUser.getUsername());
+        model.addAttribute("id", updatedUser.getId());
+        model.addAttribute("roles", updatedUser.getRoles());
+        model.addAttribute("backEndpoint", "/htmx/user/modify");
+
+        return "layouts/htmx/user_view_one :: userDetails";
+    }
+
+    @GetMapping("/delete")
+    public String deleteUsersList(Model model) {
+        var response = apiService.getAll();
+        if (response.hasError()) {
+            model.addAttribute("errorMessage", response.getError().toString());
+            model.addAttribute("windowTitle", "Error");
+            model.addAttribute("buttonText", "");
+            model.addAttribute("listItems", Map.of());
+        } else {
+            List<UserInfoDTO> users = List.of(response.getResponse());
+
+            model.addAttribute("windowTitle",
+                "Choose User to Delete");
+            model.addAttribute("buttonText", "Delete");
+
+            // Add users to list
+            BiFunction<String, Long, Map<String, String>> createListItem =
+                (description, userId) -> Map.of(
+                    "itemDescription", description,
+                    "htmxEndpoint", "/htmx/user/confirm_delete/" + userId
+                );
+            List<Map<String, String>> listItems = new ArrayList<>(users.size());
+            for (var user : users) {
+                listItems.add(
+                    createListItem.apply(user.getUsername(), user.getId())
+                );
+            }
+            model.addAttribute("listItems", listItems);
+        }
+
+        return "layouts/htmx/view_window_list :: viewWindowList";
+    }
+
+    @GetMapping("/confirm_delete/{id}")
+    public String confirmDeleteUser(Model model, @PathVariable Long id) {
+        var response = apiService.get(id);
+        if (response.hasError()) {
+            model.addAttribute("errorMessage", response.getError().toString());
+            return deleteUsersList(model);
+        }
+
+        UserInfoDTO user = response.getResponse();
+
+        model.addAttribute("windowTitle", "Confirm User Deletion:");
+        model.addAttribute("id", id);
+        model.addAttribute("name", user.getUsername());
+        model.addAttribute("cancelEndpoint", "/htmx/user/delete");
+        model.addAttribute("confirmEndpoint", "/htmx/user/delete/" + id);
+
+        return "layouts/htmx/confirm_delete :: confirmDelete";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteOneUser(Model model, @PathVariable Long id) {
+        apiService.delete(id);
+        model.addAttribute("successMessage", "User Successfully Deleted!");
+        return deleteUsersList(model);
     }
 }
