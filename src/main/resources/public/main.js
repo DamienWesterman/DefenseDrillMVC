69e18be11e11e167277f4d371067e39f8e2089ae
Damien Westerman <damien.westerman@snhu.edu>
2025-03-15 17:25:37 -0600

    Properly resize textarea

--- src/main/resources/public/main.js
+++ src/main/resources/public/main.js
@@ -1,12 +1,22 @@
+/*
+ * To be used with tab_with_window.html to highlight the currently selected tab.
+ */
 document.addEventListener("DOMContentLoaded", () => {
     const sideNavLinks = document.querySelectorAll("#sideNav a");
 
     sideNavLinks.forEach(link => {
         link.addEventListener("click", () => {
-            // Remove the 'active' class from all links
             sideNavLinks.forEach(link => link.classList.remove("active"));
-            // Add the 'active' class to the clicked link
             link.classList.add("active");
         });
     });
 });
+
+/*
+ * Dynamically resize textarea inputs as the user types
+ */
+function resizeTextarea(textarea) {
+    if (textarea.scrollHeight > textarea.clientHeight) {
+        textarea.style.height = textarea.scrollHeight + "px";
+    }
+}
